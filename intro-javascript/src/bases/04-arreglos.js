b9919e76db45d3bdc6f21dc5e85f2ea6b135e2b4
pajaro <eugenio.pajaro.ext@nokia.com>
2021-03-04 19:24:22 -0500

    Arreglos

---
+++ intro-javascript/src/bases/04-arreglos.js
@@ -0,0 +1,14 @@
+// Objetos Arreglos
+// El push modifica el objeto principal
+const arreglo = [1, 2, 3, 4];
+// arreglo.push(1);
+
+let arreglo2 = [...arreglo, 5];
+
+const arreglo3 = arreglo2.map((numero)=>{
+    return numero*2;
+});
+arreglo2.push(5);
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3);
