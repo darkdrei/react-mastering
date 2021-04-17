a5c12078ff1513980ad8b1d156b6c178b53ac94e
pajaro <eugenio.pajaro.ext@nokia.com>
2021-04-17 07:39:16 -0500

    Test de hooks

---
+++ 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useCounter } from '../../hooks/useCounter';
+import { useFetch } from '../../hooks/useFetch'
+import './examples.css'
+
+export const MultipleCustomHooks = () => {
+    
+    const { counter , increment, decrement, reset} = useCounter(1);
+    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/${ counter }`);
+    const { loading, data } = state;
+    const { author , quote } = !!data && data[0];
+    console.log( state );
+
+    return (
+        <div>
+            <h1>BreakingBad Quotes!!!</h1>
+            <hr/>
+            {
+                loading ?
+                    (
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+                    )
+                :
+                    (
+                        <blockquote className="blockquote text-center">
+                            <p className="mb-0"> { quote }</p>
+                            <footer className="blockquote-footer">{ author }</footer>
+                        </blockquote>
+                    )
+            }
+            <hr/>
+            <button className="btn btn-primary"
+                    onClick={ increment }
+            >
+                Siguiente quote
+            </button>
+        </div>
+    )
+}
